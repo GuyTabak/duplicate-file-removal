b86740b1174a1b51de42f4b8cc6c629d5391c663
Guy-PC\Guy <tbk.guy@gmail.com>
2020-04-13 22:11:25 +0300

    tests are awesome

--- duplicate_file_removal/file_record.py
+++ duplicate_file_removal/file_record.py
@@ -5,12 +5,14 @@ from os import path
 
 class Record:
     def __init__(self, full_path: str):
+        full_path = path.normpath(full_path)
         if not path.isfile(full_path):
             raise RuntimeError(f"Path provided: '{full_path}' is not valid.")
-        full_name = path.basename(full_path)
-        self.name, self.ext = path.splitext(full_name)
-        self.dir, _ = path.splitext(full_path)
-        self.full_path: str = full_path
+
+        file_name = path.basename(full_path)
+        self.name, self.ext = path.splitext(file_name)
+        self.dir = path.dirname(full_path)
+        self.full_path = full_path
         self.hash_ = self.md5_file(full_path)
 
     @staticmethod
@@ -23,9 +25,15 @@ class Record:
 
 
 class RecordsDictionary:
+    # TODO: Consider using metaclass instead of overriding all the functions.
     def __init__(self):
         self.dict_: Dict[str, List[Record]] = dict()
 
     def add(self, record: Record):
-        recorded_files = self.dict_.get(record.hash_, [])
-        recorded_files.append(record)
+        self.dict_.setdefault(record.hash_, []).append(record)
+
+    def __len__(self):
+        return len(self.dict_)
+
+    def __getitem__(self, item):
+        return self.dict_[item]
---
+++ tests/test_file_record.py
@@ -0,0 +1,47 @@
+from duplicate_file_removal.file_record import Record, RecordsDictionary
+
+from pytest import fixture
+
+from tempfile import TemporaryDirectory
+from os import path, urandom
+from hashlib import md5
+from uuid import uuid4
+
+
+@fixture(scope="module")
+def binary_data():
+    return urandom(4097)
+
+
+@fixture(scope="module")
+def temp_file_path(binary_data):
+    temp_dir = TemporaryDirectory('w+')
+    file_name = uuid4().hex + '.end'
+
+    file_ = open(path.join(temp_dir.name, file_name), 'wb')
+    file_.write(binary_data)
+    file_.close()
+
+    yield file_.name  # So that temp_dir won't be deleted until end of module execution
+
+
+@fixture(scope="module")
+def record(temp_file_path):
+    return Record(temp_file_path)
+
+
+def test_record_hash(record, binary_data):
+    assert record.hash_ == md5(binary_data).hexdigest()
+
+
+def test_record_file_path_deconstruction(record):
+    assert record.full_path == path.join(record.dir, record.name + record.ext)
+
+
+def test_dup_record_in_record_dictionary(record):
+    d = RecordsDictionary()
+    d.add(record)
+    d.add(record)
+
+    assert len(d) == 1
+    assert len(d[record.hash_]) == 2
