cc1777b9b5b3ec98a416eb12f7099012d0335913
Guy-PC\Guy <tbk.guy@gmail.com>
2020-05-10 14:42:04 +0300

    fixed tests and code

--- duplicate_file_removal/database/base_model.py
+++ duplicate_file_removal/database/base_model.py
@@ -1,10 +1,10 @@
-from os import remove
+from collections import namedtuple
+from os import remove, path
+from sqlite3 import connect, Connection, Error, Cursor
+from typing import Tuple, Optional
 
 from duplicate_file_removal import PROJECT_NAME
-from typing import Tuple, Optional
 from duplicate_file_removal.logger import logger
-from sqlite3 import connect, Connection, Error
-from collections import namedtuple
 
 
 class SQLiteTypes:
@@ -22,8 +22,10 @@ class BaseModel:
     columns: Tuple[Tuple[str, str]] = None
     primary_keys: Tuple[str] = None
     foreign_keys: Tuple[ForeignKey] = None
+    _query_separator = ","
 
-    DB_PATH = f"%AppData%\\{PROJECT_NAME}\\db\\main_db.sqlite3"  # TODO: Check compatibility with unix
+    # Won't work for unix based systems (%AppData%)
+    DB_PATH = f"%AppData%{path.sep}{PROJECT_NAME}{path.sep}db{path.sep}main_db.sqlite3"
 
     def __init__(self, db_path: Optional[str] = None):
         self.DB_PATH = db_path if db_path else self.__class__.DB_PATH
@@ -31,20 +33,21 @@ class BaseModel:
         self.create_table(self.connection)
 
     @classmethod
-    def connect(cls, path: str) -> Connection:
+    def connect(cls, path_: str) -> Connection:
         con = None
         try:
-            con = connect(path)
+            con = connect(path_)
         except Error as e:
-            logger.error(f"Error while connecting to db '{path}':\n{e}")
+            logger.error(f"Error while connecting to db '{path_}':\n{e}")
         return con
 
     @classmethod
-    def execute_query(cls, connection: Connection, query: str) -> None:
+    def execute_query(cls, connection: Connection, query: str) -> Cursor:
         cursor = connection.cursor()
         try:
             cursor.execute(query)
             connection.commit()
+            return cursor
         except Error as e:
             logger.error(f"Error while executing query '{query}':\n{e}")
 
@@ -54,35 +57,40 @@ class BaseModel:
 
     @classmethod
     def create_table(cls, conn: Connection):
-        table_creation_query = f"CREATE TABLE {cls.table_name()}" \
+        table_creation_query = f"CREATE TABLE {cls.table_name()} " \
                                f"(" \
                                f"{cls.generate_columns()}" \
                                f"{cls.generate_primary_keys()}" \
                                f"{cls.generate_foreign_keys()}" \
                                f");"
+
         cursor = conn.cursor()
         cursor.execute(table_creation_query)
         conn.commit()
 
     @classmethod
     def generate_columns(cls) -> str:
-        res = ''
-        template = "{} {},"
-        for column_name, column_type in cls.columns:
-            res += template.format(column_name, column_type)
-
-        return res
+        template = "{} {}"
+        columns = [template.format(column_name, column_type) for column_name, column_type in cls.columns]
+        return f"{cls._query_separator} ".join(columns)
 
     @classmethod
     def generate_primary_keys(cls):
-        return f"PRIMARY KEY ({tuple(cls.primary_keys)}),"
+        if not cls.primary_keys:
+            return ""
+        return "{} PRIMARY KEY ({})".format(cls._query_separator, cls._query_separator.join(cls.primary_keys))
 
     @classmethod
     def generate_foreign_keys(cls) -> str:
         res = ""
-        template = "FOREIGN KEY ({}) REFERENCES {} ({}),"
+
+        if not cls.foreign_keys:
+            return res
+
+        template = "{}FOREIGN KEY ({}) REFERENCES {} ({}) "
         for foreign_key in cls.foreign_keys:
-            res += template.format(foreign_key.source_key, foreign_key.dst_table, foreign_key.foreign_key)
+            res += template.format(cls._query_separator, foreign_key.source_key,
+                                   foreign_key.dst_table, foreign_key.foreign_key)
 
         return res
 
